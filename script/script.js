94244b3d6a9484f41aececeadfbdb5d3ac48f65d
Javier Callejón <javicallejon88@gmail.com>
2022-11-25 18:14:01 +0100

    [v1.0] Section Projects + colors

--- script/script.js
+++ script/script.js
@@ -1,8 +1,8 @@
-const projectImage = document.querySelectorAll(".project-img");
+const cardImage = document.querySelectorAll(".card-img");
 const contentBlock = document.querySelectorAll(".content-block");
 
-projectImage.forEach(( _, i) => {
-  projectImage[i].addEventListener("click", () => {
+cardImage.forEach(( _, i) => {
+  cardImage[i].addEventListener("click", () => {
     contentBlock.forEach(( _, i) => {
       contentBlock[i].classList.remove("active");
     });
