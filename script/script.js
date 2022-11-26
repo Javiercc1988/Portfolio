c7e45cb82faef668d8a3c5a0b37e1263d21b8443
Javier Callejón <javicallejon88@gmail.com>
2022-11-26 19:53:37 +0100

    [v1.0] Form Section

--- script/script.js
+++ script/script.js
@@ -1,9 +1,29 @@
 const cardImage = document.querySelectorAll(".card-img");
 const contentBlock = document.querySelectorAll(".content-block");
+const input = document.querySelectorAll(".form-input");
+const label = document.querySelectorAll(".form-label");
+const textarea = document.querySelectorAll(".form-textarea");
 
-cardImage.forEach(( _, i) => {
+input.forEach((element, i) => {
+  element.addEventListener("input", (event) => {
+    element.value
+      ? label[i].classList.add("label-fixed")
+      : label[i].classList.remove("label-fixed");
+  });
+});
+
+textarea.forEach((element, i) => {
+  element.addEventListener("input", (event) => {
+    console.log(element.value);
+    element.value
+      ? label[2].classList.add("label-fixed")
+      : label[2].classList.remove("label-fixed");
+  });
+});
+
+cardImage.forEach((_, i) => {
   cardImage[i].addEventListener("click", () => {
-    contentBlock.forEach(( _, i) => {
+    contentBlock.forEach((_, i) => {
       contentBlock[i].classList.remove("active");
     });
 
