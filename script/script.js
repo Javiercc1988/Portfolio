1b75ca04817d2fe029dfcd5272c1bea99f4ccd29
Javier Callejón <javicallejon88@gmail.com>
2022-11-26 23:23:28 +0100

    [v1.0] Contact Section Styles + button animated

--- script/script.js
+++ script/script.js
@@ -3,6 +3,36 @@ const contentBlock = document.querySelectorAll(".content-block");
 const input = document.querySelectorAll(".form-input");
 const label = document.querySelectorAll(".form-label");
 const textarea = document.querySelectorAll(".form-textarea");
+const button = document.querySelector("#send-button");
+const buttonText = document.querySelector("#button-text");
+const buttonIconSend = document.querySelector("#button-icon-send");
+const buttonIconCheck = document.querySelector("#button-icon-check");
+
+function sendButonStyles() {
+  button.addEventListener("click", () => {
+    console.log(button);
+    buttonIconSend.classList.toggle("clicked");
+
+    setTimeout(() => {
+      console.log("Entro en el 1");
+      buttonText.classList.add("text-disabled");
+      button.classList.add("sended");
+      buttonIconCheck.classList.add("sended");
+    }, 1000);
+
+    setTimeout(() => {
+      console.log("Entro en el 2");
+      buttonIconSend.classList.remove("clicked");
+      buttonIconCheck.classList.remove("sended");
+    }, 3000);
+
+    setTimeout(() => {
+      console.log("Entro en el 3");
+      buttonText.classList.remove("text-disabled");
+      button.classList.remove("sended");
+    }, 4000);
+  });
+}
 
 input.forEach((element, i) => {
   element.addEventListener("input", (event) => {
@@ -30,3 +60,5 @@ cardImage.forEach((_, i) => {
     contentBlock[i].classList.add("active");
   });
 });
+
+sendButonStyles();
