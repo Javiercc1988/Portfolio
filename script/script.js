9f99be3c33a531333b175e0a92c1d4599952c79e
Javier Callejón <javicallejon88@gmail.com>
2022-11-28 20:43:18 +0100

    [v1.0] Sidenav + hamburguer + Button to top

--- script/script.js
+++ script/script.js
@@ -2,6 +2,18 @@
 const cardImage = document.querySelectorAll(".card-img");
 const contentBlock = document.querySelectorAll(".content-block");
 
+const sidenav = document.querySelector(".sidenav");
+const toggle = document.querySelector(".toggle");
+const btn = document.querySelector("#checkbox");
+
+const buttonGoTopPage = document.querySelector("#button-top-page");
+buttonGoTopPage.onclick = () => window.scrollTo({ top: 0 });
+
+btn.addEventListener("click", () => {
+  console.log("click");
+  sidenav.classList.toggle("close");
+});
+
 cardImage.forEach((_, i) => {
   cardImage[i].addEventListener("click", () => {
     contentBlock.forEach((_, i) => {
